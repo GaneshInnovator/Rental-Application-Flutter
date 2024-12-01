7ec06fdb4c4b3376fc8540129188b1dcbccc8752
Ganesh P <128501278+GaneshInnovator@users.noreply.github.com>
2024-12-01 17:26:45 +0530

    Update location_card.dart

--- lib/widgets/location_card.dart
+++ lib/widgets/location_card.dart
@@ -1,70 +1,50 @@
 import 'package:flutter/material.dart';
-import '../utils/constants.dart';
 import '../utils/AppsConstants.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-class AdsSection extends StatelessWidget {
-  const AdsSection({Key? key}) : super(key: key);
+class LocationCard extends StatelessWidget {
+  final Map<String, String> location;
+
+  const LocationCard({Key? key, required this.location}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        for (var ad in adsImages)
-          Padding(
-            padding: const EdgeInsets.only(bottom: AppConstants.spacingBetweenElements),
-            child: Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(AppConstants.cardRadius),
-                child: Stack(
-                  children: [
-                    CachedNetworkImage(
-                      imageUrl: ad['imageUrl']!,
-                      height: 220,
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      fit: BoxFit.cover,
-                      placeholder: (context, url) => CircularProgressIndicator(),errorWidget:  (context, url, error) => Icon(Icons.error),
-                    ),
-                    Positioned(
-                      left: 16,
-                      top: 16,
-                      child: Text(
-                        AppConstants.hostingFeeText,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 1),
-                              blurRadius: 4,
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      left: 16,
-                      top: 50,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.redAccent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        child: const Text(AppConstants.becomeHostButtonText, style: TextStyle(color: Colors.white),),
-                      ),
-                    ),
-                  ],
+    return Padding(
+      padding: const EdgeInsets.only(right: AppConstants.spacingBetweenElements),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(AppConstants.cardRadius), // Rounded corners for the card
+        child: Stack(
+          children: [
+
+            CachedNetworkImage(
+              imageUrl: location['imageUrl']!,
+              width: 110,
+              height: 150,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => CircularProgressIndicator(),errorWidget:  (context, url, error) => Icon(Icons.error),
+            ),
+
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.symmetric(vertical: 6),
+                color: Colors.transparent,
+                child: Text(
+                  location['name']!,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-      ],
+          ],
+        ),
+      ),
     );
   }
 }
