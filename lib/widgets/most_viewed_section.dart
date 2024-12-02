3e00e65cb90f790befb9b996f1abc93a0f897e74
Ganesh P <128501278+GaneshInnovator@users.noreply.github.com>
2024-12-03 00:41:43 +0530

    Update most_viewed_section.dart

--- lib/widgets/most_viewed_section.dart
+++ lib/widgets/most_viewed_section.dart
@@ -8,6 +8,9 @@ class MostViewedSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -15,97 +18,111 @@ class MostViewedSection extends StatelessWidget {
           AppConstants.mostViewedText,
           style: AppConstants.subHeadingTextStyle,
         ),
-        const SizedBox(height: 16),
+        SizedBox(height: screenHeight * 0.02),
         for (var place in viwedPlaces)
           Padding(
-            padding: const EdgeInsets.only(bottom: AppConstants.spacingBetweenElements), 
+            padding: EdgeInsets.only(bottom: screenHeight * 0.02),
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(AppConstants.cardRadius),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              borderRadius: BorderRadius.circular(screenWidth * 0.03),
+              child: Stack(
                 children: [
-                  Stack(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CachedNetworkImage(
-                        imageUrl: place['imageUrl']!,
-                        height: 200,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                        placeholder: (context, url) => CircularProgressIndicator(),errorWidget:  (context, url, error) => Icon(Icons.error),
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(screenWidth * 0.03),
+                          topRight: Radius.circular(screenWidth * 0.03),
+                          bottomLeft: Radius.circular(screenWidth * 0.03),
+                          bottomRight: Radius.circular(screenWidth * 0.03),
+                        ),
+                        child: CachedNetworkImage(
+                          imageUrl: place['imageUrl']!,
+                          height: screenHeight * 0.25,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) => const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                        ),
                       ),
-                      Positioned(
-                        top: 10,
-                        right: 10,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.favorite_border,
-                            color: AppConstants.greyColor,
-                          ),
+                      SizedBox(height: screenHeight * 0.01),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  place['price']!,
+                                  style: TextStyle(
+                                    fontSize: screenWidth * 0.045,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: screenWidth * 0.02),
+                                Text(
+                                  ' / ${place['day']}',
+                                  style: TextStyle(
+                                    fontSize: screenWidth * 0.03,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(width: screenWidth * 0.02),
+                                CustomPaint(
+                                  size: Size(screenWidth * 0.02, screenHeight * 0.01),
+                                  painter: PolygonPainter(),
+                                ),
+                                const Spacer(),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.redAccent,
+                                  size: screenWidth * 0.035,
+                                ),
+                                Text(
+                                  ' ${place['rating']}',
+                                  style: TextStyle(fontSize: screenWidth * 0.03),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: screenHeight * 0.005),
+                            Text(
+                              place['title']!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: screenWidth * 0.035,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              place['detail']!,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontSize: screenWidth * 0.03,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              place['price']!,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              ' / ${place['day']}',
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            CustomPaint(
-                              size: Size(8, 8),
-                              painter: PolygonPainter(),
-                            ),
-                            const Spacer(),
-                            const Icon(
-                              Icons.star,
-                              color: Colors.redAccent,
-                              size: 14,
-                            ),
-                            Text(
-                              ' ${place['rating']}',
-                              style: const TextStyle(fontSize: 12),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          place['title']!,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black,
-                          ),
-                        ),
-                        Text(
-                          place['detail']!,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[600],
-                          ),
-                        ),
-                      ],
+                  Positioned(
+                    top: screenHeight * 0.02,
+                    right: screenWidth * 0.03,
+                    child: Container(
+                      padding: EdgeInsets.all(screenWidth * 0.01),
+                      decoration: BoxDecoration(
+                        color: AppColors.iconBackgroundColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.favorite_border,
+                        color: AppColors.iconColor,
+                        size: screenWidth * 0.08,
+                      ),
                     ),
                   ),
                 ],
