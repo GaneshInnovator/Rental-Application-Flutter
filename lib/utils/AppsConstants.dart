00d8c987ef9f5a31aaded397311a43d3c26ee423
Ganesh P <128501278+GaneshInnovator@users.noreply.github.com>
2024-12-03 00:40:00 +0530

    Update AppsConstants.dart

--- lib/utils/AppsConstants.dart
+++ lib/utils/AppsConstants.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppConstants {
-  // App Information
   static const String appName = "Rental App";
 
-  // Texts
   static const String exploreText = "Explore the world! By Travelling";
   static const String searchHintText = "Where did you go?";
   static const String popularLocationsText = "Popular locations";
@@ -13,18 +11,10 @@ class AppConstants {
   static const String hostingFeeText = "Hosting Fee for as low as 1%";
   static const String becomeHostButtonText = "Become a Host";
 
-  // Dimensions
-  static const double defaultPadding = 16.0;
-  static const double cardRadius = 12.0;
-  static const double searchHeight = 50.0;
-  static const double spacingBetweenElements = 16.0;
-
-  // Colors
   static const Color headerBackgroundColor = Color(0xFFF8F7FD);
   static const Color greyColor = Color(0xFF868B88);
   static const Color starColor = Colors.amber;
 
-  // Text Styles
   static const TextStyle headerTextStyle = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.bold,
@@ -38,8 +28,8 @@ class AppConstants {
     fontSize: 15,
     fontWeight: FontWeight.bold,
   );
-
 }
+
 class AppColors {
   static const Color cardBackgroundColor = Colors.white;
   static const Color textPrimaryColor = Colors.black;
@@ -49,13 +39,36 @@ class AppColors {
 }
 
 class AppDimensions {
-  static const double recommendedCardWidth = 250.0;
-  static const double recommendedCardImageHeight = 150.0;
-  static const double borderRadiusMedium = 12.0;
-  static const double spacingSmall = 8.0;
-  static const double spacingMedium = 16.0;
-  static const double spacingExtraSmall = 4.0;
-  static const double textLarge = 18.0;
-  static const double textMedium = 14.0;
-  static const double iconSmall = 16.0;
+  static double getRecommendedCardWidth(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.6;
+
+  static double getRecommendedCardImageHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height * 0.2;
+
+  static double getDefaultPadding(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.04;
+
+  static double getCardRadius(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.03;
+
+  static double getSearchHeight(BuildContext context) =>
+      MediaQuery.of(context).size.height * 0.07;
+
+  static double getSpacingSmall(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.02;
+
+  static double getSpacingMedium(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.04;
+
+  static double getSpacingExtraSmall(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.01;
+
+  static double getTextLarge(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.05;
+
+  static double getTextMedium(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.035;
+
+  static double getIconSmall(BuildContext context) =>
+      MediaQuery.of(context).size.width * 0.04;
 }
